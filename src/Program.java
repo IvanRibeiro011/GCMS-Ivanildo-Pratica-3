bac9b7d1adb61d3c7784d02b5e883d80d7751a66
Ivanildo Ribeiro <75269330+IvanRibeiro011@users.noreply.github.com>
2022-05-31 12:22:32 -0300

    Primeiro commit

---
+++ src/Program.java
@@ -0,0 +1,16 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World!");
+		
+		System.out.println("Segundo Commit");
+		
+		System.out.println("Terceiro commit!");
+		
+		System.out.println("teste");
+
+	}
+
+}
